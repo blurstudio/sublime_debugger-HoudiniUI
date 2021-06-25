98216798f14cc9c448beb8169f29ee4c6eb47383
Philippe Cutillas <pcutillas@email.arizona.edu>
2021-06-24 17:18:31 -0700

    Fixed bug related to recent Debugger restructure

--- attach.py
+++ attach.py
@@ -5,9 +5,6 @@ This script adds the the containing package as a valid debug adapter in the Debu
 
 """
 
-from Debugger.modules.debugger.adapter.adapters import Adapters
-from .adapter.houdini_ui import HoudiniUI
-
 import sublime
 
 
@@ -15,7 +12,4 @@ if sublime.version() < '4000':
 	raise Exception('This version of the Houdini UI adapter requires Sublime Text 4. Use the st3 branch instead.')
 
 
-def plugin_loaded():
-    """ Show message on success or fail """
-
-    Adapters.all.append(HoudiniUI())
+from .adapter.houdini_ui import HoudiniUI
